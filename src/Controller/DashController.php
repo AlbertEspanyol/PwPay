ac654e5abe01966a34d6b44cb26e438f3a8eba8e
SabrosóCubà <albert.espanol@students.salle.url.edu>
2020-05-11 18:26:14 +0200

    Dash Comments

--- src/Controller/DashController.php
+++ src/Controller/DashController.php
@@ -23,24 +23,38 @@ final class DashController {
 
         $id = $_SESSION['user_id'];
 
+        //S'agafe el path i si existeix se li afegeix ../ al principi perque sino no funcione
         $path = $this->container->get('user_repository')->getInfoById('pfp_path', $id);
-        if($path == 'Unknown') $path = "https://placehold.it/400x400"; else $path = '../' . $path;
+        if($path == 'Unknown') {
+            $path = "https://placehold.it/400x400";
+        } else {
+            $path = '../' . $path;
+        }
+
+        //S'agafen les dades de la bbdd
         $iban = $this->container->get('user_repository')->getInfoById('iban', $id);
         $money = $this->container->get('user_repository')->getInfoById('money', $id);
         $tss = $this->container->get('transaction_repository')->getLatest5Trans($id);
         $email = $this->container->get('user_repository')->getInfoById('email', $id);
 
+        //Array que guarda els mails que ens interessa mostrar a partir de les ids
         $names = [];
+        //Array que guarda les source_id i les dest_id de la taula transactions perque si se li passe directament l'array tss al twig no funcione ves a saber perque
         $srcs = [];
         $dsts = [];
 
         if(!empty($tss)){
             for($i = 0; $i < sizeof($tss); $i++){
                 $xd = $tss[$i];
+
+                //Es fiquen les ids que toquen
                 $srcs[$i] = $xd->getSourceUser();
                 $dsts[$i] = $xd->getDestUser();
+
+
                 $src = $xd->getSourceUser();
 
+                //Si l'usuari ha estat el que ha efectuat la transaccio s'ha d'agafar el mail del destinatari per a mostrar-lo
                 if($src == $id) $src = $xd->getDestUser();
 
 
@@ -53,6 +67,11 @@ final class DashController {
             $iban = "You haven't linked your bank account yet";
         }
 
+        /*******
+        * Per mostrar les transtaccions es segueix la següent logica
+        * Si es una transaccio request el destinatari es qui perd els diners
+        * Si es una transaccio send el que l'envia es qui perd els diners
+        **********/
         return $this->container->get('view')->render(
             $response,
             'dash.twig',
